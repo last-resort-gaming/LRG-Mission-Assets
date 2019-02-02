aa136f7f92e3dd28e05c33b39eb0eba79d7d8124
Mokka <mokkamichi.99@gmail.com>
2019-02-02 18:32:19 +0100

    Restructure, first document added

--- Addons/Secret01/CfgMusic.hpp
+++ Addons/Secret01/CfgMusic.hpp
@@ -4,67 +4,67 @@ class CfgMusic
 	class alien_horn_1
 	{
 		name		= "Alien Horn";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\alien_horn_1.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\alien_horn_1.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class ambient_sound_1
 	{
 		name		= "Ambient Sound 1";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\ambient_sound_1.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient_sound_1.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class car_crash
 	{
 		name		= "Car Crash";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\car_crash.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\car_crash.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class fast_heartbeat
 	{
 		name		= "Fast Heartbeat";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\fast_heartbeat.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\fast_heartbeat.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class low_drone_ambient
 	{
 		name		= "Low Drone Ambient";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\low_drone_ambient.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\low_drone_ambient.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class low_drone_ambient_2
 	{
 		name		= "Low Drone Ambient 2";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\low_drone_ambient_2.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\low_drone_ambient_2.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class reverse_voice_female
 	{
 		name		= "Reversed Voice (Female)";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\reverse_voice_female.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\reverse_voice_female.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class reverse_voice_male
 	{
 		name		= "Reversed Voice (Male)";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\reverse_voice_male.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\reverse_voice_male.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class slow_heartbeat
 	{
 		name		= "Slow Heartbeat";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\slow_heartbeat.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\slow_heartbeat.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class slow_male_breathing
 	{
 		name		= "";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\slow_male_breathing.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\slow_male_breathing.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 	class woman_breathing
 	{
 		name		= "";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\woman_breathing.ogg", 1, 1, 100 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\woman_breathing.ogg", 1, 1, 100 };
 		titles[]	= { 0, "" };
 	};
 };
