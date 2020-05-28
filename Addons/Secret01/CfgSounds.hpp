319c72c93418819b67a0b2d62f854a64f4b2e4e5
Mokka <mokkamichi.99@gmail.com>
2020-05-28 20:33:47 +0200

    Restructure the sounds

--- Addons/Secret01/CfgSounds.hpp
+++ Addons/Secret01/CfgSounds.hpp
@@ -11,7 +11,7 @@ class CfgSounds
 	class ambient_sound_1
 	{
 		name		= "Short Ambient Sound 1";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient_sound_1.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\ambient_sound_1.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 30;
 	};
@@ -32,14 +32,14 @@ class CfgSounds
 	class low_drone_ambient
 	{
 		name		= "Low Drone Ambient";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\low_drone_ambient.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\low_drone_ambient.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 140;
 	};
 	class low_drone_ambient_2
 	{
 		name		= "Low Drone Ambient 2";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\low_drone_ambient_2.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\low_drone_ambient_2.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 100;
 	};
@@ -88,63 +88,63 @@ class CfgSounds
 	class ambient_sound_2
 	{
 		name		= "Ambient Sound (2)";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient_sound_2.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\ambient_sound_2.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 45;
 	};
 	class short_glitch_1
 	{
 		name		= "Short Glitch 1";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_1.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_1.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_2
 	{
 		name		= "Short Glitch 2";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_2.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_2.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_3
 	{
 		name		= "Short Glitch 3";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_3.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_3.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_4
 	{
 		name		= "Short Glitch 4";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_4.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_4.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_5
 	{
 		name		= "Short Glitch 5";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_5.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_5.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_6
 	{
 		name		= "Short Glitch 6";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_6.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_6.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_7
 	{
 		name		= "Short Glitch 7";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_7.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_7.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
 	class short_glitch_8
 	{
 		name		= "Short Glitch 8";
-		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\short_glitch_8.ogg", 2, 1, 200 };
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\short_glitch_8.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 15;
 	};
@@ -161,5 +161,236 @@ class CfgSounds
 		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\demo_1.ogg", 2, 1, 200 };
 		titles[]	= { 0, "" };
 		length		= 360;
+	};
+	class creepy_drone_1
+	{
+		name		= "Creepy Drone #1";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\creepy_drone.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 75 ;
+	};
+	class creepy_drone_2
+	{
+		name		= "Creepy Drone #2";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\creepy_drone_2.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 30;
+	};
+	class crazy_brain
+	{
+		name		= "Crazy Brain";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\glitches\crazy_brain.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 30;
+	};
+	class tornado_siren
+	{
+		name		= "Tornado Siren #1";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\tornado_siren.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 45;
+	};
+	class tornado_siren_2
+	{
+		name		= "Tornado Siren #2 (Long!)";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\tornado_siren_2.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 330;
+	};
+	class teeth_chatter
+	{
+		name		= "Teeth Chatter";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\teeth_chatter.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 30;
+	};
+	class drone_1
+	{
+		name		= "Drone #1";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 1_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_2
+	{
+		name		= "Drone #2";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 2_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_3
+	{
+		name		= "Drone #3";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 3_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_4
+	{
+		name		= "Drone #4";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 4_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_5
+	{
+		name		= "Drone #5";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 5_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_6
+	{
+		name		= "Drone #6";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 6_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_7
+	{
+		name		= "Drone #7";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 7_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_8
+	{
+		name		= "Drone #8";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 8_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_9
+	{
+		name		= "Drone #9";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 9_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class drone_10
+	{
+		name		= "Drone #10";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Drone 10_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class disturbing_drone
+	{
+		name		= "Disturbing Drone";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Disturbing_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class fearful_drone
+	{
+		name		= "Fearful Drone";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Fearful_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class broken_drone
+	{
+		name		= "Broken Drone";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Broken_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class death_drone
+	{
+		name		= "Death Drone";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\drones\Death_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class alien_ambient
+	{
+		name		= "Alien Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Alien_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class hate_ambient
+	{
+		name		= "Hate Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Hate_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class night_sky_ambient
+	{
+		name		= "Night Sky Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Night Sky_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class particles_ambient
+	{
+		name		= "Particles Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Particles_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class scare_ambient
+	{
+		name		= "Scare Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Scare_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class tension_ambient
+	{
+		name		= "Tension Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Tension_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class tensity_ambient
+	{
+		name		= "Tensity Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\tensity_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class warnings_ambient
+	{
+		name		= "Warnings Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Warnings_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class bends_ambient
+	{
+		name		= "Bends Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Bends_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class chiller_ambient
+	{
+		name		= "Chiller Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Chiller_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class dismay_ambient
+	{
+		name		= "Dismay Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Dismay_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class filth_ambient
+	{
+		name		= "Filth Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Filth_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
+	};
+	class growl_ambient
+	{
+		name		= "Growl Ambient";
+		sound[]		= { "z\LRG Mission Assets\addons\secret01\Data\Audio\ambient\Growl_C.ogg", 2, 1, 200 };
+		titles[]	= { 0, "" };
+		length		= 15;
 	};
 };
