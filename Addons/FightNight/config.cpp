255189a862ffce45e32c7647668344ea0ab12459
Mokka <mokkamichi.99@gmail.com>
2021-01-09 00:22:20 +0100

    Old Fight Night Stuff

---
+++ Addons/FightNight/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class LRG_FightNight
+	{
+		author = AUTHOR;
+		name = NAME;
+		url = URL;
+		units[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {"A3_UI_F","cba_main","cba_xeh", "A3_Weapons_F"};
+		version = VERSION;
+		authors[] = {"Mokka"};
+		weapons[] = {};
+	};
+};
---
+++ Addons/FightNight/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT FightNight
+
+#include "\z\LRG Mission Assets\addons\main\script_mod.hpp"
